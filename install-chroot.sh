fe7a0b7dfd9c30d927fe6d5410057cea0375d61d
Mbahal <37205553+Mbahal@users.noreply.github.com>
2021-05-14 13:39:40 +0200

    Add files via upload
    two random install files

---
+++ install-chroot.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e
+set -x
+
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+
+#maybe change the hostname?
+echo 'mbahal-archlinux' > /etc/hostname
+
+sed -i -e 's/^#\(fr_FR.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+echo 'LANG=fr_FR.UTF-8' > /etc/locale.conf
+echo "KEYMAP=fr-latin1" > /etc/vconsole.conf
+echo "FONT=eurlatgr" >> /etc/vconsole.conf
+
+systemctl enable NetworkManager
+
+export HOME_DIR=mbahal
+useradd -m  -U mbahal
+echo -e 'temp\ntemp' | passwd mbahal
+#allow mbahal to use sudo
+cat <<EOF > /etc/sudoers.d/mbahal
+Defaults:mbahal !requiretty
+mbahal ALL=(ALL:ALL) ALL
+EOF
+chmod 440 /etc/sudoers.d/mbahal
+mkdir -p /etc/systemd/network
+ln -sf /dev/null /etc/systemd/network/99-default.link
+
+#eanble sshd to have ssh access
+systemctl enable sshd
+#enable qemu-guest-agent
+systemctl enable qemu-guest-agent
+sed -i -e 's/^#Color/Color/' /etc/pacman.conf
+sed -i -e 's/^#Include = \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/mirrorlist/' /etc/pacman.conf
+sed -i -e 's/filesystems keyboard/filesystems keyboard keymap lvm2 encrypt udev/' /etc/mkinitcpio.conf
+mkinitcpio -p linux-lts
+sed -i '/GRUB_CMDLINE_LINUX=/d' /etc/default/grub
+sed -i 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
+echo GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$(blkid -s UUID -o value /dev/mapper/archlvm-root):root root=/dev/mapper/root\" >> /etc/default/grub
+grub-install --target=i386-pc --bootloader-id=arch_grub --recheck /dev/sda
+sed -i -e 's/^GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=1/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+#now let's configure the /home
+mkdir -m 700 /etc/luks-keys
+
+#génération du fichier de clé
+dd if=/dev/urandom of=/etc/luks-keys/home bs=1 count=256 status=progress
+
+# Chiffrement du home via la clé crée
+
+echo "YES" | cryptsetup luksFormat -v -c aes-xts-plain64 -s 512 -h sha512 -i 4000 /dev/archlvm/home /etc/luks-keys/home
+cryptsetup -d /etc/luks-keys/home open /dev/archlvm/home home
+mkfs.xfs /dev/mapper/home
+mount /dev/mapper/home /home
+
+
+# change fstab & crypttab, but need to see if it works not tested
+
+echo "home /dev/mapper/archlvm-home  /etc/luks-keys/home
+swap /dev/mapper/archlvm-swap  /dev/urandom  swap,cipher=aes-xts-plain64,size=256
+tmp  /dev/mapper/archlvm-tmp   /dev/urandom  tmp,cipher=aes-xts-plain64,size=256" >> /etc/crypttab
+
+echo "/dev/mapper/tmp   /tmp   tmpfs  defaults  0  0
+/dev/mapper/swap  none   swap   sw  0  0
+/dev/mapper/home  /home  xfs    defaults  0  2" >> /etc/fstab
+
+echo -e 'temp\ntemp' | passwd
+
---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ -e /dev/vda ]; then
+  device=/dev/vda
+elif [ -e /dev/sda ]; then
+  device=/dev/sda
+else
+  echo "ERROR: There is no disk available for installation" >&2
+  exit 1
+fi
+export device
+#initialize device with two partitions, one that will receive the esp (efi system partition) flag and boot file
+#the other that will hold the logical volumes a
+#let 1 MiB so that the 34 first sectors are let alone since a disk need them i can't remember why but fails otherwise
+parted --script "${device}" \
+	mklabel msdos \
+	mkpart primary ext4 1MiB 100MiB \
+	set 1 boot on \
+	mkpart primary ext4 100MiB 100% \
+	q
+#create the physical volume
+pvcreate "${device}2"
+
+#create group volume on the vg
+vgcreate archlvm "${device}2"
+
+
+#logical volumes creation
+lvcreate -L 50G -n root archlvm
+lvcreate -L 2G -n swap archlvm
+lvcreate -L 1G -n tmp archlvm
+lvcreate -l 100%FREE -n home archlvm
+
+
+#creation of the encrypted root volume, -c = cipher algorithm -s size of the key, -h hashing function -i iteration_time between two unlocks (2000 by defaults)
+echo -e "YES" | cryptsetup luksFormat -c aes-xts-plain64 -s 512 -h sha512 -i 4000 /dev/archlvm/root luks_password
+
+#open encrypted volume and mount it
+cryptsetup open -d luks_password /dev/archlvm/root root
+mkfs.xfs /dev/mapper/root
+mount /dev/mapper/root /mnt
+
+# format and mount boot/esp partition
+mkfs.ext4 "${device}1"
+mkdir /mnt/boot
+mount "${device}1"  /mnt/boot
+
+# Get the four best  mirrors, will help with the download speed
+# by speed before intalling the rest of the packages
+
+echo 'Yes' | pacman -Sy pacman-contrib
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sleep 5 #apparently needs to finish something when pulling pacman-contrib that makes rankmirrors fails sometimes
+sync
+#rankmirrors -n 4 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+sync
+# Install base packages not all aare mandatory but none are not usefull imo
+
+echo 'all' | pacstrap /mnt base base-devel lvm2 cryptsetup xfsprogs linux-lts linux-lts-headers linux-firmware zip unzip p7zip vim alsa-utils dosfstools lsb-release exfat-utils bash-completion git grub efibootmgr openssh sudo qemu-guest-agent networkmanager i3-gaps i3lock i3blocks i3status rofi lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xorg-{server,xinit,apps} xdg-user-dirs xf86-video-nouveau mesa intel-ucode xorg-fonts-type1 freetype2 gsfonts sdl_ttf ttf-{dejavu,bitstream-vera,liberation} noto-fonts-{cjk,emoji,extra} tmux xterm xf86-video-vesa xf86-video-fbdev xf86-video-qxl
+
+mkdir -p /etc/X11/xorg.conf.d/
+
+echo "Section "InputClass"
+        Identifier "Keyboard Layout"
+        MatchIsKeyboard "on"
+        Option "XkbLayout" "fr"
+        Option "XkbLayout" "latin9"
+EndSection " > /etc/X11/xorg.conf.d/00-keyboard.conf
+
+
+genfstab -Up /mnt >> /mnt/etc/fstab
+arch-chroot /mnt /bin/bash
